55e9fb5d0f353d0e954638a0458c01251b7e7ea6
colonelrascals <pdhildreth@gmail.com>
2018-05-03 07:31:59 -0500

    init

---
+++ doc/intro.md
@@ -0,0 +1,9 @@
+# Introduction to pegboard
+
+TODO: 
+* function for creating board data structure
+* display board
+* receive move
+* determine whether a move is legal
+* determine if game is over
+* tally score
---
+++ src/pegboard/core.clj
@@ -0,0 +1,9 @@
+(ns pegboard.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
