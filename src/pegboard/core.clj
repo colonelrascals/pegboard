8199fe9671223acc159281e49c2b336d1d535f70
colonelrascals <pdhildreth@gmail.com>
2018-05-08 07:57:07 -0500

    row related functions

--- src/pegboard/core.clj
+++ src/pegboard/core.clj
@@ -16,10 +16,26 @@
 (defn trianglur?
  [n]
  (= n (last (take-while #(>= n %) tri))))
+
 (trianglur? 5)
+
 (trianglar? 6)
+
+(defn row-tri 
+ [n]
+ (last (take n tri)))
+
+(row-tri 1)
+(row-tri 2)
+
+(defn row-num
+ "returns row the position belongs to. ie. pos 1 row 1, 
+ pos 2 3 in row 2"
+ [pos]
+ (inc (count (take-while #(> pos %) tri))))
+ 
+  
 
 (defn -main
   [& args]
-  (println "Get ready to play peg thing!")
-)
+  (println "Get ready to play peg thing!"))
